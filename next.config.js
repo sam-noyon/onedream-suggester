61ba59e28e8e4f0db54e501180b8bd5bef251725
sam-noyon <125744031+sam-noyon@users.noreply.github.com>
2025-09-10 20:47:36 +0200

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "Content-Type" }
+        ],
+      },
+    ];
+  },
+};
 module.exports = nextConfig;
